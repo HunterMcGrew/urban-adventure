472c42af48239d41b931aecc8c25c41aba52bbeb
Hunter McGrew <hunter109@gmail.com>
2023-03-08 17:47:53 -0600

    set up react router in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,43 +1,55 @@
+// using axios for our fetch requests
 import axios from "axios";
 import { useEffect, useState } from "react";
-import logo from "./logo.svg";
 import "./App.css";
-import Navbar from "./components/Navbar";
 import useLocalStorage from "use-local-storage";
+// import to let us use React 'Browser Router' as 'Router'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+// Component Imports
+import Navbar from "./components/Navbar";
+
 
 function App() {
 
-  const [data, setData] = useState(null);
-  const [data2, setData2] = useState(null);
-
-  //theme code
-  // const defaultDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
-  // const [theme, setTheme] = useLocalStorage("theme", defaultDark ? "dark" : "light");
-
-  // const switchTheme = () => {
-  //   const newTheme = theme === "dark" ? "light" : "dark";
-  //   setTheme(newTheme);
-  // }
-
-
-  // haven't wrote an api in awhile, testing it out
-  useEffect( () => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get("/test");
-        setData(response.data.message);
-        const response2 = await axios.get("/jane");
-        setData2(response2.data.message);
-      } catch (error) {
-        console.error(error);
-      }
-    }
-
-    fetchData();
-  }, [])
-
   return (
-    <Navbar></Navbar>
+    <div className="App" data-theme={null}>
+      <Router>
+        <Routes>
+          {/* Defining each route for pages depending on path */}
+          <Route
+            path="/"
+            element={<>
+              <Navbar />
+            </>}
+          >
+          </Route>
+          <Route 
+            path="/login"
+            element={<> 
+            {/* <> after element lets you add multiple components. */}
+              <Navbar />
+            </>}
+            >
+          </Route>
+          <Route
+            path="/signup"
+            element={<>
+              <Navbar />
+            </>}
+            >
+          </Route>
+          {/* catch all route, * is anything we haven't defined, a 404 route */}
+          <Route
+            path="*"
+            element={<>
+            <div><h2>Nothing Here</h2></div>
+            </>}
+            >
+          </Route>
+        </Routes>
+      </ Router>
+    </div>
+  
   );
 }
 
