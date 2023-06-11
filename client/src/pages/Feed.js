264bb8bd9d2aff12faaa2f4482a4d5042a3fdbde
Hunter McGrew <hunter109@gmail.com>
2023-06-11 17:59:26 -0500

    started work on a barebones feed component

--- client/src/pages/Feed.js
+++ client/src/pages/Feed.js
@@ -1,9 +1,61 @@
-import React from 'react'
+import React from "react";
 
 const Feed = () => {
-  return (
-    <div>Feed</div>
-  )
-}
+	const styles = {
+		feedContainer: {
+			width: "90%",
+		},
+		primaryBtn: {
+			width: 150,
+		},
+		favoriteBtn: {
+			width: 150,
+		},
+	};
 
-export default Feed
+	return (
+		<div
+			style={styles.feedContainer}
+			className="feedContainer container-fluid bg-secondary mt-2 rounded-4 py-2 shadow"
+		>
+			<div className="feedTitleContainer">
+				<p className="p-0 m-0 text-start fs-4 fw-bold">
+					Tatoo Apprenticeship
+				</p>
+				<p className="p-0 m-0 text-start ">Chicago, IL</p>
+				<p className="p-0 m-0 text-start">Part-Time/Full-Time</p>
+			</div>
+
+			<div className="feedInfo mt-2">
+				<p className="p-0 m-0 text-start">
+					Requirements: CPR Certification, Art Portfolio, a good
+					attitude.
+				</p>
+			</div>
+
+			{/* Need to make 2 buttons for hereeee */}
+			<div className="buttonContainer mt-3 mb-2 d-flex justify-content-evenly">
+				<div className="">
+					<button
+						type="button"
+						class="btn btn-primary shadow-sm"
+						style={styles.primaryBtn}
+					>
+						Apply
+					</button>
+				</div>
+				<div className="">
+					<button
+						type="button"
+						class="btn btn-primary shadow-sm"
+						style={styles.favoriteBtn}
+					>
+						Favorite
+					</button>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Feed;
